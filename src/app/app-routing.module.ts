d3b9811d1f30c54d45ce65499248d954af8dbf84
Wei Mu <[muweigg@gmail.com]>
2017-12-02 20:01:17 +0800

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { PageNotFoundComponent } from './components';
 import { PageViewerComponent } from './pages/page.viewer.component';
 import { PageModelerComponent } from './pages/page.modeler.component';
+import { DocComponent } from './pages/doc.component';
 
 export const appRoutingComponents = [
     PageViewerComponent,
     PageModelerComponent,
+    DocComponent,
     PageNotFoundComponent
 ]
 
@@ -14,6 +16,7 @@ export var routes: Routes = [
     { path: '', redirectTo: 'modeler', pathMatch: 'full' },
     { path: 'viewer', component: PageViewerComponent },
     { path: 'modeler', component: PageModelerComponent },
+    { path: 'doc', component: DocComponent },
     { path: '**', component: PageNotFoundComponent },
 ];
 
---
+++ src/app/pages/doc.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'page-doc',
+    templateUrl: './doc.component.html',
+    styleUrls: ['./doc.component.scss']
+})
+export class DocComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
